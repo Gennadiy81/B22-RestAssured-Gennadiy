b647033293c78fe2e0f2c8511262027a9930b0d9
ghena <ghena187@gmail.com>
2021-08-21 09:49:37 -0500

    'hello'

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -1,4 +1,42 @@
 package com.cybertek.day5;
 
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class HamcrestMatchersIntro {
+    @Test
+    public void simpleTest1(){
+
+        assertThat(5+5, is(10));
+        assertThat(5+5, equalTo(10));
+        assertThat(5+5, is(equalTo(10)));
+
+        assertThat(5+5, is(greaterThan(9)));
+
+    }
+
+    @DisplayName("Assetion with String")
+    @Test
+    public void stringHamcrest(){
+
+        String text = "B22 is learning Hamcrest";
+
+        assertThat(text, is("B22 is learning Hamcrest"));
+        assertThat(text, equalTo("B22 is learning Hamcrest"));
+        assertThat(text, is(equalTo("B22 is learning Hamcrest")));
+
+        assertThat(text, startsWith("B22"));
+
+        assertThat(text,startsWithIgnoringCase("b22"));
+        assertThat(text, endsWith("rest"));
+
+
+
+
+
+
+
+    }
 }
