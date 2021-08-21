eea20881be8be6cd7c1882957011c6457e014fcf
ghena <ghena187@gmail.com>
2021-08-21 14:55:27 -0500

    added to DBUtils

---
+++ src/test/java/com/cybertek/day5/SpartanAPIvsDB.java
@@ -0,0 +1,13 @@
+package com.cybertek.day5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SpartanAPIvsDB {
+    @DisplayName("GET one spartan from api and database")
+    @Test
+    public void testDB1() {
+
+
+    }
+}
