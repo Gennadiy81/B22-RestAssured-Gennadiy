5c0aa2f04227c50036329b2038117dc90113e718
ghena <ghena187@gmail.com>
2021-08-21 11:00:46 -0500

    'added'

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -64,5 +64,21 @@ public class HamcrestMatchersApiTest {
                 .body("teachers[0].gender",equalTo("male"));
 
     }
+    @DisplayName("GET request to teacher/all and chaining")
+    @Test
+    public void teachersTest(){
+
+        //verify Alexander,Darleen,Sean inside the all teachers
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("http://api.cybertektraining.com/teacher/all")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("teachers.firstName",hasItems("Alexander","Darleen","Sean"));
+
+
+    }
 
 }
