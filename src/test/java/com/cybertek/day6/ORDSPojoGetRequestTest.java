31606c4cf8aad3d9e26c50bf3fca62c5a8805590
ghena <ghena187@gmail.com>
2021-08-22 14:47:45 -0500

    finished

--- src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -1,12 +1,21 @@
 package com.cybertek.day6;
 
+import com.cybertek.pojo.Employee;
 import com.cybertek.pojo.Region;
+import com.cybertek.pojo.Regions;
 import com.cybertek.utilities.HRTestBase;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static io.restassured.RestAssured.get;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class ORDSPojoGetRequestTest extends HRTestBase {
 
@@ -27,7 +36,53 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
 
     @DisplayName("Get request to /employees and only get couple of values as a Pojo class")
     @Test
-    public void test
+    public void employeeGet(){
+
+        Employee employee1 = get("/employees").then().statusCode(200)
+                .extract().jsonPath().getObject("items[0]", Employee.class);
+
+        System.out.println(employee1);
+    }
+   /* send a get request to regions
+        verify that region ids are 1,2,3,4
+        verify that regions names Europe ,Americas , Asia, Middle East and Africa
+        verify that count is 4
+        try to use pojo as much as possible
+        ignore non used fields
+
+     */
+
+    @DisplayName("GET request to region only some fields test")
+    @Test
+    public void regionPojoTest(){
+        //send a get request and save everthing inside the regions object
+        //since we prepare pojo also for the all properties we dont need to use any path so as() method is enough
+        Regions regions = get("/regions").then().statusCode(200).extract().response().as(Regions.class);
+
+        //verify count is 4
+        assertThat(regions.getCount(),is(4));
+        //create empty list to store values
+        List<String> regionNames = new ArrayList<>();
+        List<Integer> regionIds = new ArrayList<>();
+
+        //get list of regions out of regions object
+        List<Region> items = regions.getItems();
+
+        //loop through each of the region, save their ids and names to empty list that we prepare
+        for (Region region : items) {
+            regionIds.add(region.getRId());
+            regionNames.add(region.getRegion_name());
+        }
+        System.out.println("regionIds = " + regionIds);
+        System.out.println("regionNames = " + regionNames);
+        //prepare expected result
+        List<Integer> expectedRegionIds = Arrays.asList(1,2,3,4);
+        List<String> expectedRegionNames = Arrays.asList("Europe", "Americas", "Asia", "Middle East and Africa");
+
+        //compare two result
+        assertThat(regionIds,is(expectedRegionIds));
+        assertThat(regionNames,is(equalTo(expectedRegionNames)));
+    }
 
 
 }
