aac9c6b8ac32e6500c763fa277c2c78a62928572
ghena <ghena187@gmail.com>
2021-08-22 11:30:00 -0500

    added new pojo class

--- src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -1,6 +1,7 @@
 package com.cybertek.day6;
 
 
+import com.cybertek.pojo.Search;
 import com.cybertek.pojo.Spartan;
 import com.cybertek.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -65,7 +67,38 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
         System.out.println("s1 = " + s1);
         System.out.println("s1.getName() = " + s1.getName());
         System.out.println("s1.getGender() = " + s1.getGender());
+    }
 
+    @Test
+    public void test3(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .extract().response();
+
+        Search searchResult = response.as(Search.class);
+
+        System.out.println(searchResult.getContent().get(0).getName());
+    }
+
+    @DisplayName("GET  /spartans/search and save as List<Spartan>")
+    @Test
+    public void test4(){
+        List<Spartan> spartanList = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .extract().jsonPath().getList("content", Spartan.class);
+
+        System.out.println(spartanList.get(1).getName());
 
     }
+
 }
