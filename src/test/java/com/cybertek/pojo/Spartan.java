b9e407d3f5aab81cf901042befd9385a2164cfd1
ghena <ghena187@gmail.com>
2021-08-24 22:10:57 -0500

    finished

--- src/test/java/com/cybertek/pojo/Spartan.java
+++ src/test/java/com/cybertek/pojo/Spartan.java
@@ -1,24 +1,33 @@
 package com.cybertek.pojo;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 
-    @Setter
-    @Getter
-    @ToString
+/*
+{
+    "id": 15,
+    "name": "Meta",
+    "gender": "Female",
+    "phone": 1938695106
+}
+ */
 
-    @JsonIgnoreProperties(value = "id", allowGetters = true)
-    public class Spartan {
-        //getter setter
-        //toString
-        private int id;
-        private String name;
-        private String gender;
-        private long phone;
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(value = "id",allowSetters = true)
 
-    }
+public class Spartan {
+    //getter setter
+    //toString
+    private int id;
+    private String name;
+    private String gender;
+    private long phone;
+
+
+}
