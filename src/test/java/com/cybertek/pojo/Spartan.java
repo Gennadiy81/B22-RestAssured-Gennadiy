27c5fbd3d32157a4098895f27aba63dcf85db977
ghena <ghena187@gmail.com>
2021-08-24 19:51:46 -0500

    added serialization tqo

--- src/test/java/com/cybertek/pojo/Spartan.java
+++ src/test/java/com/cybertek/pojo/Spartan.java
@@ -1,55 +1,24 @@
 package com.cybertek.pojo;
 
 
-public class Spartan {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-    private int id;
-    private String name;
-    private String gender;
-    private long phone;
 
+    @Setter
+    @Getter
+    @ToString
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-
+    @JsonIgnoreProperties(value = "id", allowGetters = true)
+    public class Spartan {
+        //getter setter
+        //toString
+        private int id;
+        private String name;
+        private String gender;
+        private long phone;
 
     }
-}
